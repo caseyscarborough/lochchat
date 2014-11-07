d61734c06646e881a6fbb2d5f93b44453208d90d
Casey Scarborough <caseyscarborough@gmail.com>
2014-11-06 19:07:22 -0500

    Add configuration variables for Spring Websocket plugin.
    - See https://github.com/zyro23/grails-spring-websocket#installation

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -21,6 +21,8 @@ grails.project.fork = [
   console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
 ]
 
+grails.tomcat.nio = true
+grails.tomcat.scan.enabled = true
 grails.project.dependency.resolver = "maven" // or ivy
 grails.project.dependency.resolution = {
   // inherit Grails' default dependencies
