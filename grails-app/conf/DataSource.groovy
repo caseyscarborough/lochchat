0e87deb2d6fa6a3acdae1afbc07c972a5cbf3be6
Casey Scarborough <caseyscarborough@gmail.com>
2014-11-11 14:57:18 -0500

    Fix Hibernate cache provider.

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -11,7 +11,7 @@ dataSource {
 hibernate {
   cache.use_second_level_cache = true
   cache.use_query_cache = false
-  cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory' // Hibernate 3
+  cache.region.factory_class = 'org.hibernate.cache.ehcache.EhCacheRegionFactory'
   singleSession = true // configure OSIV singleSession mode
 }
 
