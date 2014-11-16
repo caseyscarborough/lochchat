4b4dc9f191721d3b8b5c79b7ec97cc607f1ef143
Casey Scarborough <caseyscarborough@gmail.com>
2014-11-16 13:31:08 -0500

    Add Mail plugin and example EmailConfig.groovy.

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -11,6 +11,9 @@
 //    grails.config.locations << "file:" + System.properties["${appName}.config.location"]
 // }
 
+grails.config.locations = []
+grails.config.locations << "file:EmailConfig.groovy"
+
 grails.project.groupId = appName // change this to alter the default package name and Maven publishing destination
 
 // The ACCEPT header will not be used for content negotiation for user agents containing the following strings (defaults to the 4 major rendering engines)
---
+++ grails-app/conf/EmailConfig.example.groovy
@@ -0,0 +1,14 @@
+// Example Mail config for Gmail
+
+grails {
+  mail {
+    host = "smtp.gmail.com"
+    port = 465
+    username = "youracount@gmail.com"
+    password = "yourpassword"
+    props = ["mail.smtp.auth":"true",
+             "mail.smtp.socketFactory.port":"465",
+             "mail.smtp.socketFactory.class":"javax.net.ssl.SSLSocketFactory",
+             "mail.smtp.socketFactory.fallback":"false"]
+  }
+}
