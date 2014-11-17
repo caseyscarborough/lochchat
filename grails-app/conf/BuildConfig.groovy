8a7e2a78aaf9bd7f195569f344292fc8ab8af325
Casey Scarborough <caseyscarborough@gmail.com>
2014-11-17 16:29:21 -0500

    Update build version.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -6,7 +6,7 @@ grails.project.work.dir = "target/work"
 grails.project.target.level = 1.6
 grails.project.source.level = 1.6
 
-def buildVersion = "001"
+def buildVersion = "002"
 def warName = "${appName}##${appVersion}-build-${buildVersion}.war"
 grails.project.war.file = "target/$warName"
 
