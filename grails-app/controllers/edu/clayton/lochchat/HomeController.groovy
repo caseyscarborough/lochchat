7824321274e8098d4ec369330079ba18923e4362
Casey Scarborough <caseyscarborough@gmail.com>
2014-11-17 12:26:38 -0500

    When SSL is used, replace port with https protocol.

--- grails-app/controllers/edu/clayton/lochchat/HomeController.groovy
+++ grails-app/controllers/edu/clayton/lochchat/HomeController.groovy
@@ -3,7 +3,7 @@ package edu.clayton.lochchat
 class HomeController {
 
   def index() {
-    def baseUrl = createLink(controller: "chat", action: "room", absolute: true)
+    def baseUrl = createLink(controller: "chat", action: "room", absolute: true).replaceAll(/http:\/\/(.*):443\//, /https:\/\/$1\//)
     def chatroomUrl = "${baseUrl}/${UUID.randomUUID().encodeAsBase64().substring(20)}"
     [chatroomUrl: chatroomUrl]
   }
