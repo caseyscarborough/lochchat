12d8afe9042caf4e1d558fcfd38fef84f521c7d3
Casey Scarborough <caseyscarborough@gmail.com>
2014-11-16 19:18:36 -0500

    Update spring-websockets to latest version.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -80,6 +80,6 @@ grails.project.dependency.resolution = {
     //compile ":coffee-asset-pipeline:1.8.0"
     //compile ":handlebars-asset-pipeline:1.3.0.3"
 
-    compile ":spring-websocket:1.1.0"
+    compile ":spring-websocket:1.2.0"
   }
 }
