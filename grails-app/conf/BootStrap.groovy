d792ed46199d182718733e4350030ff498cb5134
Casey Scarborough <caseyscarborough@gmail.com>
2014-11-22 17:44:41 -0500

    Create upload directory on application startup.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,14 @@
 class BootStrap {
 
+  def grailsApplication
+
   def init = { servletContext ->
+
+    def uploadDir = new File(grailsApplication.config.lochchat.uploadDir)
+    if (!uploadDir.exists()) {
+      log.info("Creating upload directory at $uploadDir")
+      uploadDir.mkdirs()
+    }
   }
   def destroy = {
   }
