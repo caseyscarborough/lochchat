0e977719690b60e169657f05757492a14f8561ce
Casey Scarborough <caseyscarborough@gmail.com>
2014-11-22 19:37:24 -0500

    Create Roles on application startup if they don't yet exist.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,3 +1,5 @@
+import edu.clayton.lochchat.Role
+
 class BootStrap {
 
   def grailsApplication
@@ -8,6 +10,11 @@ class BootStrap {
     if (!uploadDir.exists()) {
       log.info("Creating upload directory at $uploadDir")
       uploadDir.mkdirs()
+    }
+
+    if (Role.count == 0) {
+      new Role(authority: "ROLE_USER").save(flush: true)
+      new Role(authority: "ROLE_ADMIN").save(flush: true)
     }
   }
   def destroy = {
