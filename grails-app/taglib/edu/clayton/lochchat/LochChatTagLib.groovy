8eaa00b32affeedeafb87c242f4544790fc49a42
Casey Scarborough <caseyscarborough@gmail.com>
2014-11-23 10:56:20 -0500

    Add gravatar taglib method.

--- grails-app/taglib/edu/clayton/lochchat/LochChatTagLib.groovy
+++ grails-app/taglib/edu/clayton/lochchat/LochChatTagLib.groovy
@@ -1,13 +1,22 @@
 package edu.clayton.lochchat
 
 class LochChatTagLib {
-    static defaultEncodeAs = [taglib:'raw']
-    static namespace = "lochchat"
+  static defaultEncodeAs = [taglib: 'raw']
+  static namespace = "lochchat"
 
-    def logHtml = { attrs ->
-        Log logInstance = attrs.logInstance
-        logInstance?.messages?.sort { it.dateCreated }?.each { message ->
-            out << '<div class="chat-text old">' + message + '</div>'
-        }
+  def logHtml = { attrs ->
+    Log logInstance = attrs.logInstance
+    logInstance?.messages?.sort { it.dateCreated }?.each { message ->
+      out << '<div class="chat-text old">' + message + '</div>'
     }
+  }
+
+  def gravatarFor = { attrs ->
+    User user = attrs.user
+    if (!attrs.width) {
+      attrs.width = 400
+    }
+    out << "<img src='https://gravatar.com/avatar/${user.email.encodeAsMD5()}?s=800' width='${attrs.width}'>"
+  }
+
 }
