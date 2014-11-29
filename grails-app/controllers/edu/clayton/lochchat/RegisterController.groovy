7f8030d360c926a1128bf8bd929d7ea320ce8b7d
Casey Scarborough <caseyscarborough@gmail.com>
2014-11-29 12:57:03 -0500

    Resolve issue with user registration.

--- grails-app/controllers/edu/clayton/lochchat/RegisterController.groovy
+++ grails-app/controllers/edu/clayton/lochchat/RegisterController.groovy
@@ -2,6 +2,7 @@ package edu.clayton.lochchat
 
 import grails.plugin.springsecurity.SpringSecurityUtils
 import grails.plugin.springsecurity.annotation.Secured
+import grails.plugin.springsecurity.authentication.dao.NullSaltSource
 import grails.plugin.springsecurity.ui.RegistrationCode
 
 @Secured(['permitAll'])
@@ -20,12 +21,38 @@ class RegisterController extends grails.plugin.springsecurity.ui.RegisterControl
       render view: 'index', model: [command: command]
       return
     }
-    super.register(command)
+
+    String salt = saltSource instanceof NullSaltSource ? null : command.username
+    def user = lookupUserClass().newInstance(firstName: command.firstName, lastName: command.lastName,
+      email: command.email, username: command.username, accountLocked: true, enabled: true)
+
+    RegistrationCode registrationCode = springSecurityUiService.register(user, command.password, salt)
+    if (registrationCode == null || registrationCode.hasErrors()) {
+      // null means problem creating the user
+      flash.error = message(code: 'spring.security.ui.register.miscError')
+      flash.chainedParams = params
+      redirect action: 'index'
+      return
+    }
+
+    String url = generateLink('verifyRegistration', [t: registrationCode.token])
+    def conf = SpringSecurityUtils.securityConfig
+    def body = conf.ui.register.emailBody
+    if (body.contains('$')) {
+      body = evaluate(body, [user: user, url: url])
+    }
+    mailService.sendMail {
+      to command.email
+      from conf.ui.register.emailFrom
+      subject conf.ui.register.emailSubject
+      html body.toString()
+    }
+
+    render view: 'index', model: [emailSent: true]
   }
 
   @Override
   def verifyRegistration() {
-
     def conf = SpringSecurityUtils.securityConfig
     String defaultTargetUrl = conf.successHandler.defaultTargetUrl
 
@@ -80,6 +107,8 @@ class RegisterCommand extends grails.plugin.springsecurity.ui.RegisterCommand {
   String lastName
 
   static constraints = {
+    firstName blank: false
+    lastName blank: false
     username blank: false, validator: { value, command ->
       if (value) {
         def User = command.grailsApplication.getDomainClass(SpringSecurityUtils.securityConfig.userLookup.userDomainClassName).clazz
